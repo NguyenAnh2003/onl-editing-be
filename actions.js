5c387ebc621559968812ebc4656664a6f54226b0
NguyenAnh2003 <cunho2803032003@gmail.com>
2023-10-03 11:40:43 +0700

    #code

---
+++ actions.js
@@ -0,0 +1,10 @@
+const ACTIONS = {
+  JOIN: 'join',
+  JOINED: 'joined',
+  DISCONNECTED: 'disconnected',
+  CODE_CHANGE: 'text-change',
+  SYNC_CODE: 'sync-text',
+  LEAVE: 'leave',
+};
+
+export default ACTIONS;
---
+++ server.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import { Server } from 'socket.io';
+import http from 'http';
+import ACTIONS from './actions.js';
+
+const PORT = 5000;
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const userSocketMap = {};
+
+const getAllConnected = (roomId) => {
+  /**
+   *
+   */
+  return Array.from(
+    io.sockets.adapter.rooms.get(roomId) || []
+  ).map((socketId) => {
+    return {
+      socketId,
+      name: userSocketMap[socketId],
+    };
+  });
+};
+
+io.on('connection', (socket) => {
+  console.log(`${socket.id} connected`);
+  socket.on(ACTIONS.JOIN, ({ roomId, name }) => {
+    userSocketMap[socket.id] = name;
+    socket.join(roomId);
+    const clients = getAllConnected(roomId);
+    console.log(clients);
+    clients.forEach((socketId) => {
+      io.to(socketId).emit(ACTIONS.JOINED, {
+        clients,
+        name,
+        socketId: socket.id
+      })
+    })
+  });
+});
+
+server.listen(PORT, () =>
+  console.log(`Listenning on port ${PORT}`)
+);
