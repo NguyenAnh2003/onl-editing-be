3609e18b29cc6f5b8644066d028c864d386171cc
NguyenAnh2003 <cunho2803032003@gmail.com>
2023-10-26 21:55:31 +0700

    #code

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -3,7 +3,10 @@ import User from '../schema/user.schema.js';
 export const userLoginService = async (name, password) => {
   try {
     const user = await User.findOne({ name: name });
-    return user;
+    return {
+      userId: user._id,
+      username: user.name
+    };
   } catch (error) {
     console.error(error);
   }
