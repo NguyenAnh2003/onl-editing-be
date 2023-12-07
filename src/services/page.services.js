4ae994dcf68ce7b7239b6e11344badd7149728cf
NguyenAnh2003 <cunho2803032003@gmail.com>
2023-12-07 11:38:40 +0700

    #code

--- src/services/page.services.js
+++ src/services/page.services.js
@@ -2,7 +2,7 @@ import Page from '../schema/page.schema.js';
 import { QuillDeltaToHtmlConverter } from 'quill-delta-to-html';
 import pdf from 'html-pdf';
 import fs from 'fs';
-import { uploadPdfService } from '../libs/upload.js';
+import { uploadPdfService } from './file.service.js';
 /** REST */
 export const createPageService = async (userId, pageName) => {
   try {
